0c2eb57f7803fdcf1d6bf4029b1269954557f22a
phwhang <pwhan002@ucr.edu>
2025-03-07 17:54:26 -0800

    Complete hw

--- homework.R
+++ homework.R
@@ -1,6 +1,10 @@
 # 259 Homework - exploratory data analysis + integrating skills
 # For full credit, answer at least 8/10 questions
-# List students working with below:
+# List students working with below: Priscilla H. Whang
+
+# clear console, environment
+cat('\014')
+rm(list = ls())
 
 library(tidyverse)
 library(lubridate)
@@ -38,32 +42,90 @@ cities <- c("Charlotte", "Los Angeles", "Houston", "Indianapolis", "Jacksonville
 #> Call the function "read_weather" 
 #> Check by reading/glimpsing a single station's file
 
+library(here)
 
+# create function
+read_weather <- function(station) {
+  file_name <- paste0(station, '.csv')
+  file_path <- file.path('us-weather-history', file_name)
+  file <- read_csv(file_path)
+  
+  file <- file %>% 
+    mutate(station = station,
+           date = as.Date(date))
+  
+  return(file)
+}
+
+# check
+read_weather('KCLT')
+glimpse(read_weather('KCLT'))
 
 # QUESTION 2
 #> Use map() and your new function to read in all 10 stations
-#> Note that because map_dfr() has been superseded, and map() does not automatically bind rows, you will need to do so in the code.
+#> Note that because map_dfr() has been superseded, 
+#> and map() does not automatically bind rows, you will need to do so in the code.
 #> Save the resulting dataset to "ds"
 
+?bind_rows
 
+# iterate read_weather through stations
+ds <- map(stations, read_weather)
+ds # list of 10
+
+# bind resulting rows
+ds <- bind_rows(ds)
+ds # 3650 obs. 14 vars.
 
 # QUESTION 3
 #> Make a factor called "city" based on the station variable
 #> (station should be the level and city should be the label)
 #> Use fct_count to check that there are 365 days of data for each city 
 
+?factor
+?fct_count
+
+# make 'city' factor
+ds <- ds %>% 
+  mutate(city = factor(station, 
+                       levels = stations, 
+                       labels = cities))
+
+# now 15 vars.
+
+# check
+fct_count(ds$city)
 
 # QUESTION 4
 #> Since we're scientists, let's convert all the temperatures to C
 #> Write a function to convert F to C, and then use mutate across to 
 #> convert all of the temperatures, rounded to a tenth of a degree
 
+# view data
+ds %>% head(20)
+names(ds)
 
+# function to convert from F to C
+convert_to_c <- function(f) {
+  c <- (f - 32) * 5/9
+  c.rounded <- round(c, 1)
+  
+  return(c.rounded)
+}
+
+# convert temps in ds
+ds <- ds %>% 
+  mutate(across(ends_with('temp'), ~ convert_to_c(.)))
 
 ### CHECK YOUR WORK
 #> At this point, your data should look like the "compiled_data.csv" file
 #> in data-clean. If it isn't, read in that file to use for the remaining
 #> questions so that you have the right data to work with.
+
+compiled_data <- read_csv('data-clean/compiled_data.csv')
+
+view(compiled_data)
+view(ds) # looks same :-)
 
 # QUESTION 5
 #> Write a function that counts the number of extreme temperature days,
@@ -74,13 +136,43 @@ cities <- c("Charlotte", "Los Angeles", "Houston", "Indianapolis", "Jacksonville
 #> (Seattle, 20, Charlotte 12, Phoenix 12, etc...)
 #> Don't save this summary over the original dataset!
 
+# function for counting days of extreme temp
+count_extreme_days <- function(data) {
+  data %>% 
+    group_by(city) %>% 
+    summarize(extreme_days = sum(actual_min_temp == record_min_temp |
+                                 actual_max_temp == record_max_temp),
+              .groups = 'drop') %>% 
+    arrange(desc(extreme_days))
+}
 
+# (not sure where/how i can use the '.' pip function?)
+
+# check (without saving)
+count_extreme_days(ds) # e.g., 20 extreme days in Seattle
 
 # QUESTION 6
 #> Pull out the month from the date and make "month" a factor
 #> Split the tibble by month into a list of tibbles 
 
+?lubridate
 
+library(lubridate)
+
+# make 'month' col
+ds <- ds %>% 
+  mutate(month = factor(month(date, label = TRUE, abbr = TRUE),
+                        levels = month.abb))
+
+# check
+glimpse(ds$month)
+table(ds$month)
+
+?split 
+
+# split into lists
+months <- split(ds, ds$month)
+months
 
 # QUESTION 7
 #> For each month, determine the correlation between the actual_precipitation
@@ -88,8 +180,31 @@ cities <- c("Charlotte", "Los Angeles", "Houston", "Indianapolis", "Jacksonville
 #> Use a for loop, and print the month along with the resulting correlation
 #> Look at the documentation for the ?cor function if you've never used it before
 
+?cor
 
+names(ds)
 
+for (month in names(months)) {
+  month_data <- months[[month]]
+    
+  # precipitation
+  print(paste(toupper(month),
+              '-- Correlation between actual and average precipitation is:', 
+              round(cor(month_data$actual_precipitation, 
+                        month_data$average_precipitation), 2)))
+  
+  # min
+  print(paste(toupper(month),
+              '-- Correlation between actual and average minimum temperature is:',
+              round(cor(month_data$actual_min_temp, 
+                        month_data$average_min_temp), 2)))
+  
+  # max
+  print(paste(toupper(month),
+              '-- Correlation between actual and average maximum temperature is:',
+              round(cor(month_data$actual_max_temp, 
+                        month_data$average_max_temp), 2)))
+}
 
 # QUESTION 8
 #> Use the Data Explorer package to plot boxplots of all of the numeric variables in the dataset
@@ -97,14 +212,30 @@ cities <- c("Charlotte", "Los Angeles", "Houston", "Indianapolis", "Jacksonville
 #> Finally, use plot_correlation to investigate correlations between the continuous variables only
 #> Check the documentation for plot_correlation for an easy way to do this
 
+library(DataExplorer)
 
+glimpse(ds)
 
+# boxplots
+ds %>% 
+  select(-station, -month) %>% 
+  plot_boxplot(., by = 'city')
+ds %>% 
+  select(-station, -city) %>% 
+  plot_boxplot(., by = 'month')
+
+# corrs
+
+?plot_correlation
+
+ds %>% 
+  select(-station, -city, -month) %>% 
+  plot_correlation(., type = 'continuous')
 
 # QUESTION 9
 #> Create a scatterplot of actual_mean_temp (y axis) by date (x axis)
 #> Use facet_wrap to make a separate plot for each city (3 columns)
 #> Make the points different colors according to month
-
 
 
 
